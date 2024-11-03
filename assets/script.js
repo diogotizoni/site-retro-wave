7ced2ff7d3df4ce896233a3c6bde7b3929806a10
DIOGO H TIZONI <diogotizoni3@gmail.com>
2024-11-02 22:43:19 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.intro-text-content', { duration: 1000 });
+sr.reveal('.textcontent', { duration: 2000 });
+sr.reveal('.grid', { duration: 2000 });
